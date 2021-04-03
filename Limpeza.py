86219345cf1c81879c37469c967fea643d83f975
Matheus Tanure <tanure.matheus@gmail.com>
2021-04-03 09:22:53 -0300

    Changes Limpeza

--- Limpeza.py
+++ Limpeza.py
@@ -20,18 +20,10 @@ for x in df.index:
 for x in df.index:
     nome_rh = df.loc[x, "nome_rh"] 
     if nome_rh in nomes_sgp:
-        df.loc[nome_rh, "nome_rh"] = ''
+        df.drop(x, inplace = True)
         nomes_sgp.remove(nome_rh)
 
-for x in df.index:
-    nome_rh = df.loc[x, "nome_rh"]
-    nome_sgp = df.loc[x, "nome_sgp"]  
-    if nome_rh == nome_sgp:
-        df.drop(x, inplace = True)
-
-i=0
-for x in nomes_sgp:
-    df.loc[i, "nome_sgp"] = x
-    i+=1 
+for x, y in zip(df.index, nomes_sgp):
+    df.loc[x, "nome_sgp"] = y    
 
 df.to_excel (r'output.xlsx',index = False)
